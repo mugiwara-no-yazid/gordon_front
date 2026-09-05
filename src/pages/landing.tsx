905573874ff74e66039616ac1c85fbd24b9a34c2
mugiwara-no-yazid <136901394+mugiwara-no-yazid@users.noreply.github.com>
2026-09-05 16:26:34 +0100

    Update landing.tsx

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,8 +1,6 @@
 import Clients from "../components/Clients";
 import { CTAFinal } from "../components/cta";
-import { Footer } from "../components/footer";
 import Hero from "../components/hero";
-import Navbar from "../components/navbar";
 import Services from "../components/nosservice";
 import Testimonials from "../components/testimonials";
 import WhyUs from "../components/whyus";
